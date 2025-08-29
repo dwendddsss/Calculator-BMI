25eecf1ccdd706324a6b5832c6ecc61ae66ed27e
dwendddsss <dewiendng13@gmail.com>
2025-08-29 21:55:48 +0700

    menambahkan function

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const heightInput = document.getElementById("height")
 const resultDiv = document.getElementById("result")
 const calculateBtn = document.getElementById("calcuteBtn")
 
+function calculateBtn() {
+    const weight = parseFloat(weightInput.value);
+    const height = parseFloat(heightInput.value);
+
+}
+
