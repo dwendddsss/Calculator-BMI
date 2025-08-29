30329dcbfd3d80a5db34a51400acc1a9fa13811c
dwendddsss <dewiendng13@gmail.com>
2025-08-29 21:51:13 +0700

    menambahkan elemen

---
+++ script.js
@@ -0,0 +1,5 @@
+const weightInput = document.getElementById("weight")
+const heightInput = document.getElementById("height")
+const resultDiv = document.getElementById("result")
+const calculateBtn = document.getElementById("calcuteBtn")
+
