f1b5adcabd03236211426c76040e2aaddd320f0b
dwendddsss <dewiendng13@gmail.com>
2025-08-30 09:32:53 +0700

    komit

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const weightInput = document.getElementById("weight")
-const heightInput = document.getElementById("height")
-const resultDiv = document.getElementById("result")
-const calculateBtn = document.getElementById("calcuteBtn")
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const resultDiv = document.getElementById("result");
+const calculateBtn = document.getElementById("calculateBtn");
 
-function calculateBtn() {
+function calculateBMI() {
     const weight = parseFloat(weightInput.value);
     const height = parseFloat(heightInput.value);
 
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        resultDiv.innerText = "⚠️ Harap masukkan Berat dan Tinggi badan dengan benar!";
+        return;
+    }
+
+    const bmi = weight / ((height / 100) ** 2);
+
+    let category = "";
+    if (bmi < 18.5) {
+        category = "Berat Badan Kurang";
+    } else if (bmi < 25) {
+        category = "Berat Badan Normal";
+    } else if (bmi < 30) {
+        category = "Kelebihan Berat Badan";
+    } else {
+        category = "Obesitas";
+    }
+
+    resultDiv.innerText = `BMI Anda : ${bmi.toFixed(1)}\nKategori: ${category}`;
 }
 
-if (isNaN(weight) || isNaN (height) || weight <= 0 || height <= 0) {
-    resultDiv.innerText = "Harap masukkan Berat dan Tinggi badan dengan benar!";
-    return;
-}
-
-const bmi = weight / ((height / 100) ** 2);
-
-let category = "";
-if (bmi < 18,49){
-    category = "Berat Badan Kurang";
-} else if (bmi < 25){
-    category = "Berat Badan Normal";
-} else if (bmi < 30) {
-    category = "Kelebihan Berat Badan";
-} else {
-    category = "Obesitas";
-}
-
-resultDiv.innerText = 'BMI Anda : ${bmi.toFixed(1)}\nKategori: ${category}';
-
+calculateBtn.addEventListener("click", calculateBMI);
